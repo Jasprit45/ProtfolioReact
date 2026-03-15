1e6d934286429da82398dd0a0365a0038aa2ba3e
Jasprit45 <singhsardarjasprit@gmail.com>
2026-03-15 22:04:12 +0530

    Added contact section logic to send email using emailJS

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
 import { RevealOnScroll } from "../RevealOnScroll";
-
+import emailjs from 'emailjs-com'
 
 export const Contact = () => {
+
+   
+
+    const [formData,setFormData] = useState({
+        name:"",
+        email:"",
+        message:""
+
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(import.meta.env.VITE_SERVICE_ID,
+               import.meta.env.VITE_TEMPLATE_ID,
+                e.target, 
+                import.meta.env.VITE_PUBLIC_KEY).then((result)=>{
+            alert("Message Sent");
+            setFormData({name:"", email:"",message:""})
+        }).catch(()=>{
+            alert("Oops! Something went wrong. Please try again.");
+        });
+    }
 
     return (
         <section id="contact"
@@ -10,13 +34,15 @@ export const Contact = () => {
                 <div className="px-4 w-150 ">
 
                     <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-500 bg-clip-text text-transparent">Get In Touch</h2>
-                    <form className="space-y-6" action="">
+                    <form className="space-y-6" onSubmit={handleSubmit} action="">
                         <div className="relative">
                             <input type="text"
                              id="name"
                              name="name"
                              required 
+                             value={formData.name}
                              placeholder="Name..."
+                             onChange={(e)=>setFormData({...formData, name:e.target.value})}
                              className="w-full bg-white/5 border border-white/10 rounded px-4 py-3
                             text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5 "/>
                         </div>
@@ -25,7 +51,9 @@ export const Contact = () => {
                              id="email"
                              name="email"
                              required 
+                             value={formData.email}
                              placeholder="example@gmail.com"
+                             onChange={(e)=>setFormData({...formData, email:e.target.value})}
                              className="w-full bg-white/5 border border-white/10 rounded px-4 py-3
                             text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5 "/>
                         </div>
@@ -33,8 +61,10 @@ export const Contact = () => {
                             <textarea type="message"
                              id="message"
                              name="message"
-                             required 
+                             required
+                             value={formData.message} 
                              rows ="5"
+                             onChange={(e)=>setFormData({...formData, message:e.target.value})}
                              placeholder="Your Message..."
                              className="w-full bg-white/5 border  border-white/10 rounded px-4 py-3
                             text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5 "/>
